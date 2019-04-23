00ecc70b797e895f67bebe7987b682ef3ca92fa7
Arsalan Akhter <aakhter@wpi.edu>
2019-04-23 13:55:13 -0400

    Added the Instance Reader Class

--- TO_InstanceReader.py
+++ TO_InstanceReader.py
@@ -1,63 +1,130 @@
-from gurobipy import *
 import random as rnd
 import math
-import numpy as np
-import pickle
-import glob
 import os
-import glob
 import sys
 import json
 
 
-with open('data.json', 'r') as f:
-    json_data = json.load(f)
+class InstanceReader:
+
+    def __init__(self, instance_file):
+        self.instance_file = instance_file
+
+    def readData(self):
+        with open(self.instance_file, 'r') as f:
+            self.json_data = json.load(f)
+        self.thisSeed = self.json_data['thisSeed']
+        self.noOfTasks = self.json_data['noOfTasks']
+        self.noOfDepots = self.json_data['noOfDepots']
+        self.noOfRobots = self.json_data['noOfRobots']
+        self.K = self.json_data['K']
+        self.T = self.json_data['T']
+        self.D = self.json_data['D']
+        self.S = self.json_data['S']
+        self.E = self.json_data['E']
+        self.N = self.json_data['N']
+
+        self.L = self.json_data['L']
+        self.vel = self.json_data['vel']
+        self.T_max = self.json_data['T_max']
+        #R = json_data['R']
+
+        self.T_loc = self.json_data['T_loc']
+        self.T_loc = {k: tuple(v) for k, v in self.T_loc.items()}
+
+        self.D_loc = self.json_data['D_loc']
+        self.D_loc = {k: tuple(v) for k, v in self.D_loc.items()}
+
+        self.S_loc = self.json_data['S_loc']
+        self.S_loc = {k: tuple(v) for k, v in self.S_loc.items()}
+
+        self.E_loc = self.json_data['E_loc']
+        self.E_loc = {k: tuple(v) for k, v in self.E_loc.items()}
+
+        self.N_loc = self.json_data['N_loc']
+        self.N_loc = {k: tuple(v) for k, v in self.N_loc.items()}
+
+        self.edges = self.json_data['edges']
+        self.edges = [tuple(n) for n in self.edges]
+        self.c = self.json_data['c']
+        self.c = {tuple(k.split(':')): v for k, v in self.c.items()}
+        self.f = self.json_data['f']
+        self.f = {tuple(k.split(':')): v for k, v in self.f.items()}
+
+        self.arcs = self.json_data['arcs']
+        self.arcs = [tuple(arc) for arc in self.arcs]
+
+        self.arc_ub = self.json_data['arc_ub']
+        self.arc_ub = {tuple(k.split(':')): v for k, v in self.arc_ub.items()}
+
+        self.k_y = self.json_data['k_y']
+        self.k_y = [tuple(arc) for arc in self.k_y]
+        return self
 
 
-thisSeed = json_data['thisSeed']
+def main():
+    min_robots = 2
+    max_robots = 2
 
-noOfTasks = json_data['noOfTasks']
-noOfDepots = json_data['noOfDepots']
-noOfRobots = json_data['noOfRobots']
-K = json_data['K']
-T = json_data['T']
-D = json_data['D']
-S = json_data['S']
-E = json_data['E']
-N = json_data['N']
+    min_depots = 1
+    max_depots = 1
 
-L = json_data['L']
-vel = json_data['vel']
-T_max = json_data['T_max']
-R = json_data['R']
+    min_tasks = 1
+    max_tasks = 3
 
-T_loc = json_data['T_loc']
-T_loc = {k: tuple(v) for k, v in T_loc.items()}
+    fuel_range_start = 50
+    # fuel_range_end = int(math.ceil(2*100*math.sqrt(2)/5)*5)
+    fuel_range_end = 60
+    fuel_range_step = 5
 
-D_loc = json_data['D_loc']
-D_loc = {k: tuple(v) for k, v in D_loc.items()}
+    Tmax_range_start = 50
+    # Tmax_range_end = int(math.ceil(2*100*math.sqrt(2)/10)*10)
+    Tmax_range_end = 70
+    Tmax_range_step = 10
 
-S_loc = json_data['S_loc']
-S_loc = {k: tuple(v) for k, v in S_loc.items()}
+    robots_range = list(range(min_robots, max_robots+1))
+    depots_range = list(range(min_depots, max_depots+1))
+    tasks_range = list(range(min_tasks, max_tasks+1))
+    fuel_range = list(range(fuel_range_start, fuel_range_end +
+                            fuel_range_step, fuel_range_step))
+    Tmax_range = list(range(Tmax_range_start, Tmax_range_end +
+                            Tmax_range_step, Tmax_range_step,))
 
-E_loc = json_data['E_loc']
-E_loc = {k: tuple(v) for k, v in E_loc.items()}
+    no_of_instances = 3
+    path_to_data_folder = os.getcwd()
+    instance_dictionary = {}
 
-N_loc = json_data['N_loc']
-N_loc = {k: tuple(v) for k, v in N_loc.items()}
+    for r in robots_range:
+        for d in depots_range:
+            for t in tasks_range:
+                for f in fuel_range:
+                    for tmax in Tmax_range:
+                        instance_folder_path_suffix = \
+                            '/data' + \
+                            '/R' + str(r) + \
+                            '/D' + str(d) + \
+                            '/T' + str(t) + \
+                            '/F' + str(f) + \
+                            '/Tmax' + str(tmax)
+                        instance_folder_path = os.path.normpath(
+                            path_to_data_folder + instance_folder_path_suffix)
+                        instance_filename_prefix = '\\R' + str(r) + \
+                            'D' + str(d) + \
+                            'T' + str(t) + \
+                            'F' + str(f) + \
+                            'Tmax' + str(tmax)
+                        for it in range(no_of_instances):
+                            curr_instance_filename = instance_filename_prefix + \
+                                'Iter' + str(it) + '.json'
+                            file_path = os.path.normpath(
+                                instance_folder_path+curr_instance_filename)
+                            instance = InstanceReader(file_path)
+                            instance_dictionary[curr_instance_filename[1:-5]
+                                                ] = instance.readData()
 
-edges = json_data['edges']
-edges = [tuple(n) for n in edges]
-c = json_data['c']
-c = {tuple(k.split(':')): v for k, v in c.items()}
-f = json_data['f']
-f = {tuple(k.split(':')): v for k, v in f.items()}
+    # Print the seed of the last read file.
+    print(instance_dictionary[curr_instance_filename[1:-5]].thisSeed)
 
-arcs = json_data['arcs']
-arcs = [tuple(arc) for arc in arcs]
 
-arc_ub = json_data['arc_ub']
-arc_ub = {tuple(k.split(':')): v for k, v in arc_ub.items()}
-
-k_y = json_data['k_y']
-k_y = [tuple(arc) for arc in k_y]
+if __name__ == "__main__":
+    main()
