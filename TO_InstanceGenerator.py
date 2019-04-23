8ffdbec52ac0406b951ef25f757d41aa1d312312
Arsalan Akhter <aakhter@wpi.edu>
2019-04-23 13:09:10 -0400

    Wrote Instance_Generator class. Generated some data.

--- TO_InstanceGenerator.py
+++ TO_InstanceGenerator.py
@@ -1,82 +1,163 @@
-from gurobipy import *
 import random as rnd
 import math
 import numpy as np
-import pickle
-import glob
 import os
-import glob
 import sys
 import json
 
-thisSeed = rnd.randrange(sys.maxsize)
 
-noOfTasks = 5
-noOfDepots = 5
-noOfRobots = 3
-K = ["K" + str(i) for i in range(noOfRobots)]
-T = ["T" + str(i) for i in range(noOfTasks)]
-D = ["D" + str(i) for i in range(noOfDepots)]
-S = ['S0']
-E = ['E0']
-N = T + D + S + E
+class Instance_Generator:
 
-L = 150
-vel = 1
-T_max = 300
-R = {task: 1 for task in T}
+    def __init__(self, no_of_robots, no_of_depots, no_of_tasks, fuel, Tmax,
+                 no_of_instances=1, path_to_data_folder=os.getcwd()):
+        self.noOfRobots = no_of_robots
+        self.noOfTasks = no_of_tasks
+        self.noOfDepots = no_of_depots
+        self.L = fuel
+        self.T_max = Tmax
+        self.vel = 1
+        self.no_of_instances = no_of_instances
+        self.instance_folder_path_suffix = \
+            '/data' + \
+            '/R' + str(self.noOfRobots) + \
+            '/D' + str(self.noOfDepots) + \
+            '/T' + str(self.noOfTasks) + \
+            '/F' + str(self.L) + \
+            '/Tmax' + str(self.T_max)
+        self.instance_folder_path = os.path.normpath(
+            path_to_data_folder + self.instance_folder_path_suffix)
+        self.instance_filename_prefix = '\\R' + str(self.noOfRobots) + \
+                                        'D' + str(self.noOfDepots) + \
+                                        'T' + str(self.noOfTasks) + \
+                                        'F' + str(self.L) + \
+                                        'Tmax' + str(self.T_max)
 
-T_loc = {task: (100*rnd.random(), 100*rnd.random()) for task in T}
-D_loc = {loc: (100*rnd.random(), 100*rnd.random()) for loc in D}
-S_loc = {loc: D_loc['D0'] for loc in S}
-E_loc = {loc: D_loc['D0'] for loc in E}
-N_loc = {**T_loc, **D_loc, **S_loc, **E_loc}
+    def create_instance(self):
+        self.thisSeed = rnd.randrange(sys.maxsize)
+        rnd.seed(self.thisSeed)
+        self.K = ["K" + str(i) for i in range(self.noOfRobots)]
+        self.T = ["T" + str(i) for i in range(self.noOfTasks)]
+        self.D = ["D" + str(i) for i in range(self.noOfDepots)]
+        self.S = ['S0']
+        self.E = ['E0']
+        self.N = self.T + self.D + self.S + self.E
 
-edges = [(i, j) for i in N for j in N if i != j]
-c = {t: np.linalg.norm(
-    np.array(N_loc.get(t[0]))-np.array(N_loc.get(t[1]))) for t in iter(edges)}
-f = c  # Just for consistency with the paper
+        # R = {task: 1 for task in T}
 
-arcs = [(i, j, k) for i in N for j in N for k in K if i != j]
-arc_ub = {(i, j, k): 1 for i in N for j in N for k in K if i != j}
-for arc in arc_ub:
-    if arc[0] in D and arc[1] in D:
-        arc_ub[arc] = noOfTasks
-    if arc[0] in E and arc[1] in S:
-        arc_ub[arc] = 0  # You cannot go from end to start
-k_y = [(i, k) for i in T for k in K]
+        self.T_loc = {task: (100*rnd.random(), 100*rnd.random())
+                      for task in self.T}
+        self.D_loc = {loc: (100*rnd.random(), 100*rnd.random())
+                      for loc in self.D}
+        self.S_loc = {loc: self.D_loc['D0'] for loc in self.S}
+        self.E_loc = {loc: self.D_loc['D0'] for loc in self.E}
+        self.N_loc = {**self.T_loc, **self.D_loc, **self.S_loc, **self.E_loc}
+
+        self.edges = [(i, j) for i in self.N for j in self.N if i != j]
+        self.c = {t: np.linalg.norm(
+            np.array(self.N_loc.get(t[0]))-np.array(self.N_loc.get(t[1])))
+            for t in iter(self.edges)}
+        self.f = self.c  # Just for consistency with the paper
+
+        self.arcs = [(i, j, k)
+                     for i in self.N for j in self.N for k in self.K if i != j]
+        self.arc_ub = {
+            (i, j, k): 1 for i in self.N for j in self.N for k in self.K if i != j}
+        for arc in self.arc_ub:
+            if arc[0] in self.D and arc[1] in self.D:
+                self.arc_ub[arc] = self.noOfTasks
+        self.k_y = [(i, k) for i in self.T for k in self.K]
+
+    def create_json_data(self):
+        self.json_data = {
+            'thisSeed': self.thisSeed,
+            'noOfTasks': self.noOfTasks,
+            'noOfDepots': self.noOfDepots,
+            'noOfRobots': self.noOfRobots,
+            'K': self.K,
+            'T': self.T,
+            'D': self.D,
+            'S': self.S,
+            'E': self.E,
+            'N': self.N,
+
+            'L': self.L,
+            'vel': self.vel,
+            'T_max': self.T_max,
+            # 'R': self.R,
+
+            'T_loc': self.T_loc,
+            'D_loc': self.D_loc,
+            'S_loc': self.S_loc,
+            'E_loc': self.E_loc,
+            'N_loc': self.N_loc,
+
+            'edges': self.edges,
+            'c': dict((':'.join(k), v) for k, v in self.c.items()),
+            'f': dict((':'.join(k), v) for k, v in self.f.items()),
+
+            'arcs': [[arc[0], arc[1], arc[2]] for arc in self.arcs],
+            'k_y': self.k_y,
+            'arc_ub': dict((':'.join(k), v) for k, v in self.arc_ub.items())
+        }
+
+    def write_instance_to_json(self, iteration_number):
+        curr_instance_filename = self.instance_filename_prefix + \
+            'Iter' + str(iteration_number) + '.json'
+        file_path = os.path.normpath(
+            self.instance_folder_path+curr_instance_filename)
+
+        if not os.path.exists(self.instance_folder_path):
+            os.makedirs(self.instance_folder_path)
+
+        with open(file_path, 'w') as fp:
+            json.dump(self.json_data, fp, sort_keys=True, indent=4)
+
+    def generate_data(self):
+        for iteration in range(self.no_of_instances):
+            self.create_instance()
+            self.create_json_data()
+            self.write_instance_to_json(iteration)
 
 
-json_data = {
-    'thisSeed': thisSeed,
-    'noOfTasks': noOfTasks,
-    'noOfDepots': noOfDepots,
-    'noOfRobots': noOfRobots,
-    'K': K,
-    'T': T,
-    'D': D,
-    'S': S,
-    'E': E,
-    'N': N,
+def main():
+    min_robots = 2
+    max_robots = 2
 
-    'L': L,
-    'vel': vel,
-    'T_max': T_max,
-    'R': R,
+    min_depots = 1
+    max_depots = 1
 
-    'T_loc': T_loc,
-    'D_loc': D_loc,
-    'S_loc': S_loc,
-    'E_loc': E_loc,
-    'N_loc': N_loc,
+    min_tasks = 1
+    max_tasks = 3
 
-    'edges': edges,
-    'c': dict((':'.join(k), v) for k, v in c.items()),
-    'f': dict((':'.join(k), v) for k, v in f.items()),
+    fuel_range_start = 50
+    # fuel_range_end = int(math.ceil(2*100*math.sqrt(2)/5)*5)
+    fuel_range_end = 60
+    fuel_range_step = 5
 
-    'arcs': [[arc[0], arc[1], arc[2]] for arc in arcs],
-    'k_y': k_y,
-    'arc_ub': dict((':'.join(k), v) for k, v in arc_ub.items())
-}
-with open('data.json', 'w') as fp:
-    json.dump(json_data, fp, sort_keys=True, indent=4)
+    Tmax_range_start = 50
+    # Tmax_range_end = int(math.ceil(2*100*math.sqrt(2)/10)*10)
+    Tmax_range_end = 70
+    Tmax_range_step = 10
+
+    robots_range = list(range(min_robots, max_robots+1))
+    depots_range = list(range(min_depots, max_depots+1))
+    tasks_range = list(range(min_tasks, max_tasks+1))
+    fuel_range = list(range(fuel_range_start, fuel_range_end +
+                            fuel_range_step, fuel_range_step))
+    Tmax_range = list(range(Tmax_range_start, Tmax_range_end +
+                            Tmax_range_step, Tmax_range_step,))
+
+    no_of_instances = 3
+
+    for r in robots_range:
+        for d in depots_range:
+            for t in tasks_range:
+                for f in fuel_range:
+                    for tmax in Tmax_range:
+                        instance = Instance_Generator(
+                            r, d, t, f, tmax, no_of_instances)
+                        instance.generate_data()
+
+
+if __name__ == "__main__":
+    main()
