ed9f5c694af59490d2a05a81756ac393ad316ebd
Arsalan Akhter <aakhter@wpi.edu>
2019-05-06 10:14:18 -0400

    initialized function create_feasible_uniform_instance

--- FeasibleUniformInstanceGenerator.py
+++ FeasibleUniformInstanceGenerator.py
@@ -1,6 +1,18 @@
 from TO_InstanceGenerator import Instance_Generator
+import os
 
 
-class FeasibleUniformInstanceGenerator(Instance_Generator):
+class Feasible_Uniform_Instance_Generator(Instance_Generator):
 
-    def __init__(self, instance_string, path_to_working_dir=os.getcwd()):
+    def __init__(self, no_of_robots, no_of_depots, no_of_tasks, fuel, Tmax,
+                 no_of_instances=1, path_to_data_folder=os.getcwd()):
+
+        Instance_Generator.__init__(self, no_of_robots, no_of_depots,
+                                    no_of_tasks, fuel, Tmax, no_of_instances=1,
+                                    path_to_data_folder=os.getcwd())
+
+    def create_feasible_uniform_instance(self, iteration):
+        self.create_instance(iteration)
+        # Check if each task node is in feasible region. If not, sample another
+        # point and check if that point is in feasible region. Ensure all
+        # points are in feasible region
