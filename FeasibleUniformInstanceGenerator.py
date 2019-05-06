096da9b9315b63e240253e34d1fd41079e1ac781
Arsalan Akhter <aakhter@wpi.edu>
2019-05-06 09:59:24 -0400

    Initiated class FeasibleUniformInstanceGenerator

---
+++ FeasibleUniformInstanceGenerator.py
@@ -0,0 +1,6 @@
+from TO_InstanceGenerator import Instance_Generator
+
+
+class FeasibleUniformInstanceGenerator(Instance_Generator):
+
+    def __init__(self, instance_string, path_to_working_dir=os.getcwd()):
